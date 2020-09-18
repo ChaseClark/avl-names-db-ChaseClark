f4779c43ec94a6f1b2d4eb047258a936553138ce
Chase J. Clark <chase_clark07@hotmail.com>
2020-09-18 16:45:45 -0400

    Implement CLI

--- src/edu/frostburg/cosc310/p00/ChaseClarkNames.java
+++ src/edu/frostburg/cosc310/p00/ChaseClarkNames.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class ChaseClarkNames implements COSC310_P00{
 
@@ -12,6 +13,12 @@ public class ChaseClarkNames implements COSC310_P00{
     private boolean isRunning = true;
     private int count = 0;
     private final String path = "src/edu/frostburg/cosc310/p00/names.txt";
+
+    // questions to ask
+    // count using traversal or global var
+    // empty find method from interface?
+    // how to check if avl tree is working properly?
+
 
     /**
      * Runs project.  You will create an instance of your project in main
@@ -28,8 +35,49 @@ public class ChaseClarkNames implements COSC310_P00{
 //        db.printTree();
         insertRecordsFromFile(path);
         System.out.println("Please enter a command or type '?' for help");
+        Scanner in = new Scanner(System.in);
+        // main input loop using a switch case for the custom commands
+        while(isRunning)
+        {
+            String input = in.nextLine();
+            // convert input into array with 1 or more elements
+            String[] args = input.trim().split(" ");
+            // switch over the first argument
+            switch (args[0]) {
+                case "":
+                    break;
+                case "find":
+                    // check for 1 or 2 args
+                    break;
+                case "add":
+                    // ensure 4 args
+                    break;
+                case "count":
+                    System.out.println(count());
+                    break;
+                case "who?":
+                    System.out.println(getMyName());
+                    break;
+                case "?":
+                    help();
+                    break;
+                case "exit":
+                    System.out.println("CCNamesDB is shutting down...goodbye");
+                    in.close();
+                    exit();
+                    break;
+                case "print": // this method is just added for debugging
+                    db.printTree();
+                    break;
+                default:
+                    System.out.printf("Command not recognized, type ? for help");
+                    break;
+            }
 
 
+        }
+        // no longer running
+        System.exit(0);
     }
 
     private void insertRecordsFromFile(String path) {
@@ -90,21 +138,34 @@ public class ChaseClarkNames implements COSC310_P00{
 
     @Override
     public int count() {
-        return 0;
+        return count;
     }
 
     @Override
     public void who() {
-
+        System.out.println(getMyName());
     }
 
     @Override
     public void help() {
-
+        System.out.println("-----------------------------------");
+        System.out.println("Available commands");
+        System.out.println("find [name] - Searches the tree for the first person with that last name and prints out " +
+                "their record if present.");
+        System.out.println("find [fname][lname] - Searches the tree for the person specified and prints their record" +
+                " if present.");
+        System.out.println("add [id][fname][lname][age] - Inserts a new record with this info or generate an error " +
+                "message if invalid.");
+        System.out.println("count - Print the number of names in the database.");
+        System.out.println("who? = Print the creator's name.");
+        System.out.println("? - Print this list of commands.");
+        System.out.println("exit - Quit the program.");
+        System.out.println("-----------------------------------");
     }
 
     @Override
     public void exit() {
+        System.out.println("Shutting down...goodbye");
         isRunning = false;
     }
 }
